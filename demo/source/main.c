3d262ac39c6247151a96831d323bae46c36f0def
173210 <root.3.173210@live.com>
2016-01-12 14:17:05 +0900

    demo: Introduce suPuts
    suPuts works without GSP events to prevent from crashes.

--- demo/source/main.c
+++ demo/source/main.c
@@ -1,33 +1,109 @@
 #include <3ds.h>
+#include <3ds/gfx.h>
+#include <errno.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include "font.h"
 #include "libsu.h"
 
+#define HEIGHT (240)
+#define WIDTH (400)
+
+#define FONT_HEIGHT (8)
+#define FONT_WIDTH (8)
+
+struct px {
+	uint8_t b;
+	uint8_t g;
+	uint8_t r;
+};
+
+static int x;
+static int y;
+
+static void suPutc(int c)
+{
+	struct px (* frame)[HEIGHT];
+	unsigned int font_x, font_y;
+	const unsigned char *p;
+
+	frame = (void *)gfxTopLeftFramebuffers[0];
+
+	switch(c) {
+		case '\n':
+			x = 0;
+			y -= FONT_HEIGHT;
+			if (y < 0)
+				y = FONT_HEIGHT;
+			break;
+
+		default:
+			p = font + c * FONT_WIDTH;
+
+			for (font_x = 0; font_x < FONT_WIDTH; font_x++) {
+				for (font_y = 0; font_y < FONT_HEIGHT; font_y++) {
+					if ((0x80 >> font_y) & *p) {
+						frame[x][y - font_y].r = 255;
+						frame[x][y - font_y].g = 255;
+						frame[x][y - font_y].b = 255;
+					} else {
+						frame[x][y - font_y].r = 0;
+						frame[x][y - font_y].g = 0;
+						frame[x][y - font_y].b = 0;
+					}
+				}
+
+				p++;
+				x++;
+			}
+	}
+}
+
+void suPuts(const char *s)
+{
+	while (*s) {
+		suPutc(*s);
+		s++;
+	}
+
+	suPutc('\n');
+}
+
 int main(int argc, char **argv) {
-    gfxInitDefault();
+	char s[32];
+
+	gfxTopLeftFramebuffers[0] = linearAlloc(WIDTH * HEIGHT * sizeof(struct px));
+	if(gfxTopLeftFramebuffers[0] == NULL)
+		return ENOMEM;
+
+	gfxSet3D(false);
+	gfxSetScreenFormat(GFX_TOP, GSP_BGR8_OES);
+	gfxConfigScreen(GFX_TOP, true);
+
+	x = 0;
+	y = HEIGHT - 1;
+
 	sdmcInit();
-    consoleInit(GFX_TOP, NULL);
 	
-	printf("Arm11 exploit demo, get access to am:u service\n\n");
+	suPuts("Arm11 exploit demo, get access to am:u service\n");
     if(suInit() == 0)
 	{
 		Result res = amInit();
-		printf("am:u init : %08X\n", res);
+		sprintf(s, "am:u init : %08lX", res);
+		suPuts(s);
 	}
 	
-    printf("Press START to exit.\n");
+	suPuts("Press START to exit.");
 
     while(aptMainLoop()) {
         hidScanInput();
         if(hidKeysDown() & KEY_START) {
             break;
         }
-
-        gfxFlushBuffers();
-        gfxSwapBuffers();
-        gspWaitForVBlank();
     }
 
-    gfxExit();
+	linearFree(gfxTopLeftFramebuffers[0]);
 	sdmcExit();
 	amExit();
     return 0;
